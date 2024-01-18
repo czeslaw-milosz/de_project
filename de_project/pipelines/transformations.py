0772825e3b40f2f77bec3ffd4acda520b4a8ba7c
Radoslaw Jurczak <radoslawjurczak95@gmail.com>
2024-01-18 01:38:57 +0100

    Add functionality for silver and gold layer creation
    Signed-off-by: Radoslaw Jurczak <radoslawjurczak95@gmail.com>

--- de_project/pipelines/transformations.py
+++ de_project/pipelines/transformations.py
@@ -1,7 +1,5 @@
 import datetime
-from typing import Any
 
-import deltalake
 import prefect
 import pyspark
 from prefect.runtime import flow_run, task_run
@@ -10,75 +8,62 @@ from pyspark.context import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
-from pyspark.sql.types import StringType
+
+from de_project import config
+from de_project.pipelines import utils
 
 
 @prefect.task
-def standardize_raw_data(df: pyspark.sql.DataFrame):
-    """Standardize dataframe of raw crawl data.
+def preprocess_crawl(spark: pyspark.sql.SparkSession, lake_name: str, table_name: str) -> str:
+    """Preprocess raw crawl data to the format used by the silver layer of the data lake.
 
-    1. Replace missing value strings with None.
-    2. Drop mis-scraped rows with missing values in the `offer_id` or `price_total` column (such data is unusable by any other pipeline).
-    3. Replace artifact newline and \r characters in the `short_description` and `description` columns with spaces.
+    1. Standardize dataframe of raw crawl data.
+    2. Deduplicate dataframe of scraped data.
+    3. Apply shared preprocessing.
+    4. Apply site-specific preprocessing.
 
     Args:
-        df (pyspark.sql.DataFrame): Input dataframe.
+        table_url (str): URL of the Delta Lake table with raw crawl data.
 
     Returns:
-        pyspark.sql.DataFrame: Standardized dataframe.
+        str: URL of the Delta Lake table with preprocessed crawl data.
     """
-    return df.replace(
-        {"": None, "BRAK INFORMACJI": None, "MISSING": None}
-        ).dropna(
-            subset=["offer_id", "price_total"]
-        ).withColumns({
-            "short_description": f.regexp_replace(f.col("short_description"), r'[\n\r]', " "),
-            "description": f.regexp_replace(f.col("description"), r'[\n\r]', " "),
-        })
+    df = spark.read.load(f"s3a://{lake_name}/{table_name}")
+    df = utils.deduplicate(utils.standardize_raw_data(df))
+    df = utils.preprocess_crawl_shared(df)
+    df = utils.preprocess_crawl_site_specific(df)
+    df = utils.ensure_schema(df, config.SILVER_TABLE_SCHEMA)
+    output_table_url = f"s3a://{lake_name}/silver/crawl_data"
+    df.write.format("delta").mode("append").save(output_table_url)
+    return output_table_url
 
 
 @prefect.task
-def deduplicate(df: pyspark.sql.DataFrame):
-    """Deduplicate dataframe of scraped data (input is assumed to be standardized as in `standardize_raw_data`).
-
-    For now, only basic deduplication based on exact matching of `offer_id` or `short_description` is performed.
-    In the future, more advanced deduplication methods based on images similarity should be implemented.
-
-    Args:
-        df (pyspark.sql.DataFrame): Input dataframe.
-
-    Returns:
-        pyspark.sql.DataFrame: Deduplicated dataframe.
-    """
-    return df.dropDuplicates(
-        subset=["offer_id"]
-        ).dropDuplicates(
-            subset=["short_description"]
-        )
+def create_business_analytics_table(spark: pyspark.sql.SparkSession, lake_name: str, silver_table_name: str) -> str:
+    df = spark.read.load(
+        f"s3a://{lake_name}/silver/{silver_table_name}"
+    ).select(
+        "offer_date", "city", "price_total", "price_per_msq", "size",
+    ).dropna(
+        subset=["price_total", "price_per_msq"]
+    ).orderBy("offer_date")
+    output_table_url = f"s3a://{lake_name}/gold/price_analytics"
+    df.write.format("delta").mode("overwrite").save(output_table_url)
+    return output_table_url
 
 
 @prefect.task
-def clean_data(df: pyspark.sql.DataFrame):
-    """Clean dataframe of scraped data (input is assumed to be standardized as in `standardize_raw_data`).
-
-    1. Convert prices to floats.
-    2. Convert floor numbers to integers (0 = ground floor, -1 = basement, 11 = above 10th floor).
-    3.Convert m^2 sizes to floats.
-
-    Args:
-        df (pyspark.sql.DataFrame): Input dataframe.
-
-    Returns:
-        pyspark.sql.DataFrame: Standardized dataframe.
-    """
-    df = df.replace(
-            {"Parter": "0", "Suterena": "-1", "Powyżej 10": "11"}, 
-            subset=["floor"]
-        )
-    return df.withColumns({
-        "floor": f.col("floor").cast("int"),
-        "price_total": f.regexp_replace(f.regexp_replace(f.col("price_total"), r" zł", ""), r" ", "").cast("float"),
-        "price_per_msq": f.col("price_per_msq").cast("float"),
-        "size": f.col("size").cast("float"),
-        })
-
+def create_ml_dataset(spark: pyspark.sql.SparkSession, lake_name: str, silver_table_name: str) -> str:
+    df = spark.read.load(
+        f"s3a://{lake_name}/silver/{silver_table_name}"
+    ).select(
+        *config.ML_FEATURES_SUBSET
+    ).dropna(
+        subset="price_total"
+    ).withColumns({
+        "weekday_posted": f.dayofweek(f.col("offer_date")),
+        "month_posted": f.month(f.col("offer_date")),
+    })
+    output_table_url = f"s3a://{lake_name}/gold/ml_dataset"
+    df.write.format("delta").mode("overwrite").save(output_table_url)
+    return output_table_url
